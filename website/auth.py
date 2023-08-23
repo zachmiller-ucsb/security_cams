41211681dcfa586fda71d9afccaf993b839b7c37
zachmiller-ucsb <zmiller@umail.ucsb.edu>
2023-08-23 16:04:03 -0700

    first commit, added frontend files for login, signup, home pages, as well as templates

---
+++ website/auth.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, request, flash 
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if password1 != password2:
+            flash('Passwords do not match', category='error')
+        else:
+            flash('Account created!', category='success')
+    return render_template("sign_up.html")
+
