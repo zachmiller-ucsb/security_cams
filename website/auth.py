d8efd2b2aae617c4bf86dfe8d1300c5e2205440d
zachmiller-ucsb <zmiller@umail.ucsb.edu>
2023-08-26 16:18:33 -0700

    added authentication

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,34 @@
-from flask import Blueprint, render_template, request, flash 
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User 
+from passlib.hash import sha256_crypt
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if sha256_crypt.verify(password, user.password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('Email does not exist', category='error')
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -18,9 +38,17 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if password1 != password2:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
+        elif password1 != password2:
             flash('Passwords do not match', category='error')
         else:
+            new_user = User(email=email, first_name=firstName, password=sha256_crypt.encrypt(password1))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash('Account created!', category='success')
-    return render_template("sign_up.html")
+            return redirect(url_for('views.home'))
+    return render_template("sign_up.html", user=current_user)
 
