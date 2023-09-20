6d8810c1a9eb6778c433d9870b0fdb251fbe7265
zachmiller-ucsb <zmiller@umail.ucsb.edu>
2023-09-20 12:34:51 -0700

    added endpoint to load videos when component on timeline is clicked. Incorporated into frontend with event handling

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, send_from_directory
+from flask import Blueprint, render_template, send_from_directory, jsonify, url_for, request
 from flask_login import login_required, current_user
 from . import db
 from .models import File
@@ -13,5 +13,13 @@ def home():
 
 @views.route('/uploads/<path:filename>', methods=['GET'])
 @login_required
-def download_file(filename):
+def get_video(filename):
     return send_from_directory(directory='/Users/zachary/Desktop/security_cams/instance/video/', path=filename, as_attachment=True)
+
+@views.route('/generate_url', methods=['POST'])
+@login_required
+def generate_url():
+    data = request.get_json()
+    filename = data['filename']
+    url = url_for('views.get_video', filename=filename)
+    return jsonify({'url': url})
