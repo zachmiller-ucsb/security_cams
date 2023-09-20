5ab6a4d19e09d2c0fd62e9503004e14677d62abb
zachmiller-ucsb <zmiller@umail.ucsb.edu>
2023-09-20 10:44:04 -0700

    displays video below timeline given custom path. Will have to change for deployment

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 from flask_login import login_required, current_user
 from . import db
 from .models import File
@@ -10,3 +10,8 @@ views = Blueprint('views', __name__)
 def home():
     files = db.session.query(File.filename, File.camera_id, File.startat, File.endat).all()
     return render_template("home.html", user=current_user, data=files)
+
+@views.route('/uploads/<path:filename>', methods=['GET'])
+@login_required
+def download_file(filename):
+    return send_from_directory(directory='/Users/zachary/Desktop/security_cams/instance/video/', path=filename, as_attachment=True)
